bcb45534dea59fdb3b4e6079cc6103854df13b3d
Diego Koch <d-koch@hotmail.com>
2023-02-11 21:41:35 -0300

    QuintoEjercicioOpenBootcamp

--- cuartoEjercicio.js
+++ cuartoEjercicio.js
@@ -1,3 +1,5 @@
+
+
 /* Crea un archivo JS que contenga las siguientes líneas
 
 - Una variable que contenga tu altura en centímetros (entero)
@@ -7,16 +9,20 @@
 - Una variable que contenga tu peso en kilogramos (número de coma flotante)
 
 - Una variable que contenga tu altura en metros redondeada hacia arriba
-
+2
 - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
 
 - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
  */
 
 
-altura = 1.89;
+let altura = 1.89;
+let peso = 89.6
+let alturaNumeroEntero = console.log(parseInt(altura));
+let alturaNumeroFlotante = console.log(parseFloat(altura));
+let pesoNumeroFlotante = console.log(parseFloat(peso));
+let alturaRedondeadaHaciaArriba = console.log(altura.toFixed());
+let pesoRedondeadoHaciaAbajo = console.log(Math.floor(peso));
+let maxValorJs = Number.MAX_VALUE
+let pruebaMaxValorJs = console.log(maxValorJs + 1);
 
-console.log(parseInt(altura));
-console.log(parseFloat(altura));
-
-console.log("Diego");
