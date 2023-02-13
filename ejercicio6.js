a7cb5918df311dd69b32cefecbddd2ba91c4106f
Diego Koch <d-koch@hotmail.com>
2023-02-13 16:20:41 -0300

    ejercicio Set OpenBootcamp

---
+++ ejercicio6.js
@@ -0,0 +1,14 @@
+
+let array = ["Fernando", "Nora", "Leandro", "Diego", "Belen","Kyle"];
+let miSet = new Set(array);
+
+
+console.log(miSet);
+
+let agregandoMiNombre = miSet.add("Diego");
+
+console.log(agregandoMiNombre)
+
+let agregandoJavascript = miSet.add("Javascript");
+
+console.log(agregandoJavascript);
